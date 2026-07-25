8c9e359081409c59aebf38c9b2143b9e7f439f83
Terrence <liuterrence4@gmail.com>
2026-07-25 17:37:06 -0500

    feat: add SHabitat Module Variable

--- SimulationManager/Simulation/Simulation_Easy/SimulationEasy.js
+++ SimulationManager/Simulation/Simulation_Easy/SimulationEasy.js
@@ -1,12 +1,20 @@
 
-
 const TPS = 20;
 
 function Update() {
     let Power = 0 //Power covered in watts
     let Money = 10000000 //In Dolla dollars
-    console.log("Tick Updated")
+    //Modules
+    
+    const SHabitat_Module = {
+        Name: "Small Habitat Module",
+        Cost: 6000000,
+        PowerConsumption: 80,
+        CrewCapacity: 2
+    };
+
+    console.log("Tick Updated");
 }
 
 
-setInterval(Update, 1000 / TPS)
+setInterval(Update, 1000 / TPS);
